ee31badfc2809e328dc3c997533553a8072aa549
Guppy Train <guppytrain.login@gmail.com>
2018-08-11 23:33:55 -0700

    added docker.cmd file to be triggered by sdlatest

--- alpine_dev/build.sh
+++ alpine_dev/build.sh
@@ -9,8 +9,12 @@ sudo docker build -t "${USER}/${IMAGE}:${VERSION_MAJOR}${VERSION_MINOR}${MAJOR_M
 
 if [ -n "$1" ]; then
     echo "Applying custom tag: \"$1\""
-    sudo docker tag  "${USER}/${IMAGE}:${VERSION_MAJOR}${VERSION_MINOR}${MAJOR_MICRO}" "$1"
+    sudo docker tag "${USER}/${IMAGE}:${VERSION_MAJOR}${VERSION_MINOR}${MAJOR_MICRO}" "$1"
+
+    echo "sudo docker run -it $1 bash" > "$HOME/dev/docker/docker.cmd"
 else
+    echo "sudo docker run -it "${USER}/${IMAGE}:${VERSION_MAJOR}${VERSION_MINOR}${MAJOR_MICRO}" bash" > "$HOME/dev/docker/docker.cmd"
+
     printf \
         "\n\n%s\n\n%s\n\n" \
         "No custom tag applied.  Tag this image with the following command:" \
