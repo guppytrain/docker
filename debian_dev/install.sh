3ca6f9a1637400c0157e6cfdfb3715bef5344475
Guppy Train <guppytrain.login@gmail.com>
2018-08-27 09:27:07 -0700

    from sh to bash; after install source the share_profile and run base install

--- debian_dev/install.sh
+++ debian_dev/install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 echo "Starting apt updates"
 
@@ -12,10 +12,23 @@ echo "Done with apt updates"
 cd /root/dev
 git clone https://github.com/guppytrain/planter.git
 
+# strip sudo if not there 
+if [ -z "$(which sudo)" ]; then 
+    cd /root/dev/linux
+    rp.sh -a sudo bin etc
+fi
+
 # run setup
 cd /root/dev/planter
 [ -f "devseeds.sh" ] && ./devseeds.sh
 
 cd /root/dev/linux/bin
 [ -f "setup.sh" ] && ./setup.sh -n
+
+. "$HOME/share/etc/.share_profile"
+
+cd /root/dev/linux/bin # yea, yea...one extra cd, move on.
+[ -f "base_install.sh" ] && ./base_install.sh
+
+if [ -n "$(which bash)" ]; then exec bash; else exec ${SHELL:-sh}; fi
 
