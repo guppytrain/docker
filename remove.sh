b79ba637aa97d1bd4b841fe265b92cbab3e83036
Guppy Train <guppytrain.login@gmail.com>
2018-08-27 23:28:48 -0700

    fix variable typo

--- remove.sh
+++ remove.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 . ./CONFIG
+. ./CONFIG.volume
 
 # remove share config file
 rm "${SHARE_CONFIG_FILE}"
 
 # TODO: remove shared volume?
-# rm -rf "${SHARE_DIR}/docker"
+# rm -rf "${SHARED_VOLUME}"
