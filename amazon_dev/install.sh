7599d74d28b4938a53f12de2826382865f686a01
Guppy Train <guppytrain.login@gmail.com>
2018-08-28 12:40:11 -0700

    expand amazonlinux installs

--- amazon_dev/install.sh
+++ amazon_dev/install.sh
@@ -1,21 +1,15 @@
 #!/bin/sh
 
-echo "Starting apt updates"
-
-apt-get upgrade
-apt-get install -y apt-utils
-
-apt-get install -y git
-
-echo "Done with apt updates"
-
+# get planter
 cd /root/dev
 git clone https://github.com/guppytrain/planter.git
 
 # run setup
 cd /root/dev/planter
 [ -f "devseeds.sh" ] && ./devseeds.sh
+[ -f "duso.sh" ] && ./duso.sh
 
 cd /root/dev/linux/bin
 [ -f "setup.sh" ] && ./setup.sh -n
+[ -f "base_install.sh" ] && ./base_install.sh -n
 
