6749ce7ec773872d4349d359b4dca015efd52a1b
Guppy Train <guppytrain.login@gmail.com>
2018-09-16 13:32:28 -0700

    clean up setup and remove script for .docker

--- remove.sh
+++ remove.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 . ./CONFIG
+. ./CONFIG.bash
 . ./CONFIG.volume
 
 # remove share config file
 rm "${SHARE_CONFIG_FILE}"
 
-# TODO: remove shared volume?
+# TODO: remove shared volume? NAH, for now
 # rm -rf "${SHARED_VOLUME}"
--- setup.sh
+++ setup.sh
@@ -12,13 +12,18 @@ else
     echo "Reusing existing shared volume: "${SHARED_VOLUME}""
 fi
 
+# clean slate for share config file
+echo "" > "${SHARE_CONFIG_FILE}"
+
 # cat config(s) in the share config file
-echo "# Docker shell config and vars" > "${SHARE_CONFIG_FILE}"
+echo "# Docker shell config and vars" >> "${SHARE_CONFIG_FILE}"
 cat "${VOLUME_CONFIG_FILE}" >> "${SHARE_CONFIG_FILE}"
+
+printf "\n" >> "${SHARE_CONFIG_FILE}"
 
 # bash completion
 # cat completion wrappers in the share config file
-echo "# Docker machine and compose completion helpers" > "${SHARE_CONFIG_FILE}"
+echo "# Docker machine and compose completion helpers" >> "${SHARE_CONFIG_FILE}"
 cat "${BASH_CONFIG_FILE}" >> "${SHARE_CONFIG_FILE}"
 
 
